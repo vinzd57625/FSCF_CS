87c4930283e2009615799fbfdc461fe45348c467
vinzd57625 <vdel_rosario.57625@farnborough.ac.uk>
2015-11-03 10:09:16 +0000

    3/11/2015

---
+++ src/Java2/Age.java
@@ -0,0 +1,21 @@
+package Java2;
+
+import java.util.*;
+
+public class Age {
+
+	public static void main(String[] args) {
+		int age = 0;
+		int inputValue=0;
+		
+		inputValue=sc.nextInt();
+		
+		While(inputValue!=999);{
+		
+		age=age+inputValue;
+        System.out.println("Another value");
+        inputValue=sc.nextInt();
+        
+	}
+
+}
---
+++ src/Java2/WhileLoops.java
@@ -0,0 +1,16 @@
+package Java2;
+
+public class WhileLoops {
+
+	public static void main(String[] args) {
+		
+		int x =8;
+		while(x>8){
+			System.out.println("in the loop");
+		}
+		System.out.println("Past the loop");
+		}
+
+	}
+
+
